cdf9dc57848b15ca06e9fb44a9f1e25ab6ecb603
GeetaPriyadarshini <priyadarshinigeeta@gmail.com>
2018-07-26 18:50:07 +0530

    Myblog

--- myblog/polls/views.py
+++ myblog/polls/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from .models import Question
+from django.template import loader
 
 
 def index(request):
@@ -8,6 +9,15 @@ def index(request):
 	latest_question_list = Question.objects.order_by('-pub_date')[:5]
 	output = ', '.join([q.question_text for q in latest_question_list])
 	return HttpResponse(output)
+    
+def index(request):
+	latest_question_list = Question.objects.order_by('-pub_date')[:5]
+	template = loader.get_template('polls/index.html')
+	context = {
+	'latest_question_list': latest_question_list,
+	}
+	return HttpResponse(template.render(context, request))
+
 
 def detail(request, question_id):
 	return HttpResponse("You are looking at question %s." % question_id)
